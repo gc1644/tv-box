fdf1619ae9eaf97872754f9733bf9443c6c822fe
gc1644 <gleb@duck.com>
2026-08-20 19:40:50 +0300

    path

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from library import scan_library
 from gui import TVBox
 
 
-library = scan_library("/home/gleb/Downloads/tv-box/media")
+library = scan_library("/home/kotki/Videos")
 
 app = TVBox(library)
 app.run()
