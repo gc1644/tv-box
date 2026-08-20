bfc7c2e3f01d08cf58070e01e2b7bcc13be8223b
gc1644 <gleb@duck.com>
2026-08-20 19:53:08 +0300

    dslkfj

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from pathlib import Path
+
 from library import scan_library
 from gui import TVBox
 
 
-library = scan_library("/home/kotki/Videos")
+library = scan_library(
+    Path.home() / "Videos"
+)
 
 app = TVBox(library)
 app.run()
